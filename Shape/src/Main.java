1814aeeb1016f92bd9900c465dce97358cc02867
Makra-Pov <151705461+Makra-Pov@users.noreply.github.com>
2024-01-11 08:09:14 +0700

    Add files via upload

---
+++ Shape/src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+    double l=10;
+    double w=15;
+    double side=20;
+    Rectangle r =new Rectangle(w,l);
+    System.out.println("Area of Rectangle:"+r.getArea());
+    System.out.println("Perimeter of Rectangle:"+r.getPerimeter());
+
+    System.out.println("\n");
+
+    Square s=new Square(side);
+    System.out.println("Area of Square:"+s.getArea());
+    System.out.println("Perimeter of Square:"+s.getPerimeter());
+
+
+
+
+
+    }
+}
